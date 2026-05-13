d7623567b35aa1a6dc4e0109e89f21055804a6cb
GRSilva01 <gr.dsilva0121@gmail.com>
2026-05-13 18:53:39 -0300

    base spring boot

---
+++ appPacer/src/main/java/com/pacer/appPacer/AppPacerApplication.java
@@ -0,0 +1,13 @@
+package com.pacer.appPacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPacerApplication.class, args);
+	}
+
+}
---
+++ appPacer/src/test/java/com/pacer/appPacer/AppPacerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacer.appPacer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPacerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
